6f8b8b0539e1a8dd7c39bd78bf106139cc960215
mathiasritter <mr2n17@soton.ac.uk>
2019-01-28 20:47:31 +0000

    added simple test

---
+++ src/test/java/uk/ac/soton/comp2211/MainTest.java
@@ -0,0 +1,15 @@
+package uk.ac.soton.comp2211;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
