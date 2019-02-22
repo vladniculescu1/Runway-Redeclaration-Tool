d655f26c0edd2c9347cb70623451776a2dbbdd99
mathiasritter <mr2n17@soton.ac.uk>
2019-02-22 14:04:01 +0000

    added javadoc to arrow and chevron series

--- src/main/java/uk/ac/soton/comp2211/draw/ArrowSeries.java
+++ src/main/java/uk/ac/soton/comp2211/draw/ArrowSeries.java
@@ -4,8 +4,20 @@ import uk.ac.soton.comp2211.model.RunwaySide;
 
 import java.awt.geom.Path2D;
 
+/**
+ * A shape that represents a series of consecutive arrows.
+ */
 public class ArrowSeries extends Path2D.Double {
 
+    /**
+     * A series of arrows starting from a given position, with a given with. The runway side adjust the direction
+     * in which the arrows are drawn.
+     *
+     * @param startPos the starting position
+     * @param width the width of each arrrow
+     * @param length the length of the series (all arrows)
+     * @param side the side of the runway
+     */
     public ArrowSeries(double startPos, double width, double length, RunwaySide side) {
 
         switch (side) {
--- src/main/java/uk/ac/soton/comp2211/draw/ChevronSeries.java
+++ src/main/java/uk/ac/soton/comp2211/draw/ChevronSeries.java
@@ -4,8 +4,20 @@ import uk.ac.soton.comp2211.model.RunwaySide;
 
 import java.awt.geom.Path2D;
 
+/**
+ * A shape that represents a series of consecutive chevrons.
+ */
 public class ChevronSeries extends Path2D.Double {
 
+    /**
+     * A series of chevrons starting from a given position, with a given with. The runway side adjust the direction
+     * in which the chevrons are drawn.
+     *
+     * @param startPos the starting position
+     * @param width the width of each chevron
+     * @param length the length of the series (all chevrons)
+     * @param side the side of the runway
+     */
     public ChevronSeries(double startPos, double width, double length, RunwaySide side) {
 
         switch (side) {
