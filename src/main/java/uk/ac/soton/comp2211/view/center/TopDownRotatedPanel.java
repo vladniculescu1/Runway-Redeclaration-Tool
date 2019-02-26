a5dc7902771d7a4b2f2c145bb2cb21427b3dd8ee
Paras <paras.h.404@gmail.com>
2019-02-26 16:46:22 +0000

    Merge remote-tracking branch 'remotes/origin/master' into feature/rotate-runway
    # Conflicts:
    #	src/main/java/uk/ac/soton/comp2211/Main.java

--- src/main/java/uk/ac/soton/comp2211/view/center/TopDownRotatedPanel.java
+++ src/main/java/uk/ac/soton/comp2211/view/center/TopDownRotatedPanel.java
@@ -1,9 +1,8 @@
 package uk.ac.soton.comp2211.view.center;
 
-import org.apache.commons.logging.Log;
 import uk.ac.soton.comp2211.draw.DrawExecutor;
 import uk.ac.soton.comp2211.model.LogicalRunway;
-import uk.ac.soton.comp2211.model.PhysicalRunway;
+import uk.ac.soton.comp2211.model.RunwaySelection;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,12 +17,14 @@ public class TopDownRotatedPanel extends JPanel {
 
     private DrawExecutor drawExecutor;
     private LogicalRunway logicalRunway;
+    private RunwaySelection runwaySelection;
+
 
     /**
      * Constructs a new top down rotated panel.
      */
-    public TopDownRotatedPanel(DrawExecutor drawExecutor) {
-
+    public TopDownRotatedPanel(RunwaySelection runwaySelection, DrawExecutor drawExecutor) {
+        this.runwaySelection = runwaySelection;
         this.drawExecutor = drawExecutor;
     }
 
@@ -31,7 +32,6 @@ public class TopDownRotatedPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g.create();
-
         BufferedImage image = this.getImage();
 
         // create the transform, note that the transformations happen in reversed order (so check them backwards)
@@ -39,8 +39,8 @@ public class TopDownRotatedPanel extends JPanel {
         // 3. translate it to the center of the component
         at.translate(this.getWidth() / 2, this.getHeight() / 2);
         // 2. do the actual rotation
-//        at.rotate(Math.PI);
-        at.rotate(Math.PI/2);
+//        at.rotate(0);
+        at.rotate(Math.toRadians(90+(runwaySelection.getSelectedRunway().getLowerThreshold().getHeading())*10));
         // 1. translate the object so that you rotate it around the center
         at.translate(-image.getWidth()/2, -image.getHeight()/2);
 
@@ -52,8 +52,11 @@ public class TopDownRotatedPanel extends JPanel {
      * @return the image that is created of the runway
      */
     private BufferedImage getImage() {
-        BufferedImage image = new BufferedImage(this.getWidth(), this.getHeight(), Transparency.TRANSLUCENT);
+
+        BufferedImage image = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = image.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, // Anti-alias!
+                RenderingHints.VALUE_ANTIALIAS_ON);
         this.drawExecutor.executeDrawers(g2d, this.getWidth(), this.getHeight());
         g2d.dispose();
         return image;
