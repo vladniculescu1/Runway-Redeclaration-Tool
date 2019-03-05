86e0a683ee92dd70823ed5f4346dd179a233a763
AJPutland <adam.putland@btinternet.com>
2019-03-05 15:11:18 +0000

    Merge remote-tracking branch 'remotes/origin/master' into improvement/calculator-refactor
    # Conflicts:
    #	src/main/java/uk/ac/soton/comp2211/draw/DesignatorDrawer.java

---
+++ src/main/java/uk/ac/soton/comp2211/controller/ShowCalculationController.java
@@ -0,0 +1,25 @@
+package uk.ac.soton.comp2211.controller;
+
+import uk.ac.soton.comp2211.view.MainFrame;
+import uk.ac.soton.comp2211.view.modal.ShowCalculationFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ShowCalculationController implements ActionListener {
+    private ShowCalculationFrame showCalculationFrame;
+    private MainFrame mainFrame;
+
+    public ShowCalculationController() {
+        this.showCalculationFrame = new ShowCalculationFrame(mainFrame);
+
+    }
+    public void addMainFrame(MainFrame mainFrame)  {
+        this.mainFrame = mainFrame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
