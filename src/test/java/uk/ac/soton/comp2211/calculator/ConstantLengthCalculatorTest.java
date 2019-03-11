141b3d5e9ab2b47ec123d303a9b2551564e88284
mathiasritter <mr2n17@soton.ac.uk>
2019-03-11 16:51:03 +0000

    finished constant length calculator tests

--- src/test/java/uk/ac/soton/comp2211/calculator/ConstantLengthCalculatorTest.java
+++ src/test/java/uk/ac/soton/comp2211/calculator/ConstantLengthCalculatorTest.java
@@ -126,7 +126,7 @@ class ConstantLengthCalculatorTest {
     }
 
     @Test
-    void getRunwayMarginLength_noStopWayNoClearwayLower() {
+    void getRunwayMarginLength_noStopWayNoClearway() {
         PhysicalRunway runway = Runways.noStopWayNoClearway();
         var calculator = runway.getConstantLengthCalculator();
         var actualMargin = calculator.getRunwayMarginLength(RunwaySide.LOWER_THRESHOLD);
@@ -135,47 +135,152 @@ class ConstantLengthCalculatorTest {
     }
 
     @Test
-    void getRunwayMarginLength_noStopWayNoClearwayHigher() {
-        PhysicalRunway runway = Runways.noStopWayNoClearway();
+    void getRunwayMarginLength_displacedThresholdNoStopwayClearwayLower() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayClearwayLower();
         var calculator = runway.getConstantLengthCalculator();
+        var expectedMargin = calculator.getClearwayLength(RunwaySide.LOWER_THRESHOLD);
         var actualMargin = calculator.getRunwayMarginLength(RunwaySide.HIGHER_THRESHOLD);
 
-        assertEquals(60, actualMargin);
+        assertEquals(expectedMargin, actualMargin);
     }
 
     @Test
-    void getRunwayMarginLength_noStopWayClearwayLower() {
-        PhysicalRunway runway = Runways.noStopWayNoClearway();
+    void getRunwayMarginLength_displacedThresholdNoStopwayClearwayHigher() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayClearwayHigher();
         var calculator = runway.getConstantLengthCalculator();
-        var expectedMargin = 60 + calculator.getClearwayLength(RunwaySide.HIGHER_THRESHOLD);
+        var expectedMargin = calculator.getClearwayLength(RunwaySide.HIGHER_THRESHOLD);
         var actualMargin = calculator.getRunwayMarginLength(RunwaySide.LOWER_THRESHOLD);
 
         assertEquals(expectedMargin, actualMargin);
     }
 
     @Test
-    void getRunwayMarginLength_noStopWayClearwayHigher() {
+    void getRunwayLength_noStopWayNoClearway() {
         PhysicalRunway runway = Runways.noStopWayNoClearway();
         var calculator = runway.getConstantLengthCalculator();
-        var expectedMargin = 60 + calculator.getClearwayLength(RunwaySide.LOWER_THRESHOLD);
-        var actualMargin = calculator.getRunwayMarginLength(RunwaySide.HIGHER_THRESHOLD);
+        var expectedLength = runway.getLowerThreshold().getOriginalTora();
+        var actualLength = calculator.getRunwayLength();
 
-        assertEquals(expectedMargin, actualMargin);
+        assertEquals(expectedLength, actualLength);
     }
 
     @Test
-    void getRunwayLength() {
+    void getRunwayLength_displacedThresholdStopwayClearwayLower() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayNoClearwayLower();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getLowerThreshold().getOriginalTora();
+        var actualLength = calculator.getRunwayLength();
+
+        assertEquals(expectedLength, actualLength);
     }
 
     @Test
-    void getStripLength() {
+    void getRunwayLength_displacedThresholdStopwayClearwayHigher() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayNoClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getHigherThreshold().getOriginalTora();
+        var actualLength = calculator.getRunwayLength();
+
+        assertEquals(expectedLength, actualLength);
     }
 
     @Test
-    void getDisplacedThresholdLength() {
+    void getStripLength_noStopWayNoClearway() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayNoClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = 2 * calculator.getStripMargin() + calculator.getRunwayLength();
+        var actualLength = calculator.getStripLength();
+
+        assertEquals(expectedLength, actualLength);
     }
 
     @Test
-    void getTotalVisualisationLength() {
+    void getStripLength_displacedThresholdStopwayClearwayLower() {
+        PhysicalRunway runway = Runways.displacedThresholdStopwayClearwayLower();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = 2 * calculator.getStripMargin() + calculator.getStopwayLength(RunwaySide.LOWER_THRESHOLD)
+            + calculator.getRunwayLength() + calculator.getStopwayLength(RunwaySide.HIGHER_THRESHOLD);
+        var actualLength = calculator.getStripLength();
+
+        assertEquals(expectedLength, actualLength);
     }
+
+    @Test
+    void getStripLength_displacedThresholdStopwayClearwayHigher() {
+        PhysicalRunway runway = Runways.displacedThresholdStopwayClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = 2 * calculator.getStripMargin() + calculator.getStopwayLength(RunwaySide.LOWER_THRESHOLD)
+                + calculator.getRunwayLength() + calculator.getStopwayLength(RunwaySide.HIGHER_THRESHOLD);
+        var actualLength = calculator.getStripLength();
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void getDisplacedThresholdLength_noStopWayNoClearwayLower() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayNoClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getLowerThreshold().getOriginalTora() - runway.getLowerThreshold().getOriginalLda();
+        var actualLength = calculator.getDisplacedThresholdLength(RunwaySide.LOWER_THRESHOLD);
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void getDisplacedThresholdLength_noStopWayNoClearwayHigher() {
+        PhysicalRunway runway = Runways.displacedThresholdNoStopwayNoClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getHigherThreshold().getOriginalTora() - runway.getHigherThreshold().getOriginalLda();
+        var actualLength = calculator.getDisplacedThresholdLength(RunwaySide.HIGHER_THRESHOLD);
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void getDisplacedThresholdLength_displacedThresholdClearwayStopwayLower() {
+        PhysicalRunway runway = Runways.displacedThresholdStopwayClearwayLower();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getLowerThreshold().getOriginalTora() - runway.getLowerThreshold().getOriginalLda();
+        var actualLength = calculator.getDisplacedThresholdLength(RunwaySide.LOWER_THRESHOLD);
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void getDisplacedThresholdLength_displacedThresholdClearwayStopwayHigher() {
+        PhysicalRunway runway = Runways.displacedThresholdStopwayClearwayHigher();
+        var calculator = runway.getConstantLengthCalculator();
+        var expectedLength = runway.getHigherThreshold().getOriginalTora() - runway.getHigherThreshold().getOriginalLda();
+        var actualLength = calculator.getDisplacedThresholdLength(RunwaySide.HIGHER_THRESHOLD);
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void getResa_noStopWayNoClearway() {
+        PhysicalRunway runway = Runways.noStopWayNoClearway();
+        var calculator = runway.getConstantLengthCalculator();
+        var actualResa = calculator.getResa();
+
+        assertEquals(240, actualResa);
+    }
+
+    @Test
+    void getBlastProtection_noStopWayNoClearway() {
+        PhysicalRunway runway = Runways.noStopWayNoClearway();
+        var calculator = runway.getConstantLengthCalculator();
+        var actualBlastProtection = calculator.getBlastProtection();
+
+        assertEquals(300, actualBlastProtection);
+    }
+
+    @Test
+    void getStripMargin_noStopWayNoClearway() {
+        PhysicalRunway runway = Runways.noStopWayNoClearway();
+        var calculator = runway.getConstantLengthCalculator();
+        var actualStripMargin = calculator.getStripMargin();
+
+        assertEquals(60, actualStripMargin);
+    }
+
 }
