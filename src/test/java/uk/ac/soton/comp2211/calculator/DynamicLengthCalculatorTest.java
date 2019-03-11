3484b285a27d4eb83697cae7c5e03d5b050539e1
AJPutland <adam.putland@btinternet.com>
2019-03-11 17:07:22 +0000

    Merge remote-tracking branch 'remotes/origin/master' into feature/breakdown-of-calculations
    # Conflicts:
    #	src/main/java/uk/ac/soton/comp2211/calculator/DynamicLengthCalculator.java

--- src/test/java/uk/ac/soton/comp2211/calculator/DynamicLengthCalculatorTest.java
+++ src/test/java/uk/ac/soton/comp2211/calculator/DynamicLengthCalculatorTest.java
@@ -1,13 +1,22 @@
 package uk.ac.soton.comp2211.calculator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import uk.ac.soton.comp2211.model.PhysicalRunway;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DynamicLengthCalculatorTest {
 
+    @BeforeAll
+    void setUp(){
+     //   runways = new Runways();
+    }
+
     @Test
     void getTora() {
+        PhysicalRunway physicalRunway = Runways.stopWayClearwayHigher();
+     //   physicalRunway.g
     }
 
     @Test
