6a2125737ea4ab258a79e9c14e66257de00d181e
joennn <swiftorry@gmail.com>
2019-03-18 15:10:56 +0000

    Fixes #74

---
+++ src/main/java/uk/ac/soton/comp2211/model/validate/LdaSumCheck.java
@@ -0,0 +1,31 @@
+package uk.ac.soton.comp2211.model.validate;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {LdaSumCheckValidator.class})
+public @interface LdaSumCheck {
+
+    /**
+     * The message that will be attached to a constraint violation in case of a failed validation.
+     *
+     * @return message indicating failed validation.
+     */
+    String message();
+
+    /**
+     * @return the groups the validation constraint belongs to.
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * @return payload used for extendability.
+     */
+    Class<? extends Payload>[] payload() default {};
+}
