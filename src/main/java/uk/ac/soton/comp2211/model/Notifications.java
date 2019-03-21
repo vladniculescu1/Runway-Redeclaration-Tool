1181fb04f72a13d5def7980912a42311075857c8
Vlad <danvladadrian@yahoo.com>
2019-03-21 14:55:22 +0000

    implemented timer.

---
+++ src/main/java/uk/ac/soton/comp2211/model/Notifications.java
@@ -0,0 +1,10 @@
+package uk.ac.soton.comp2211.model;
+
+/**
+ * The constants for notifications.
+ */
+public enum Notifications {
+    
+    ASSIGN_OBSTACLE, ADD_RUNWAY
+
+}
--- src/main/java/uk/ac/soton/comp2211/view/south/NotificationsPanel.java
+++ src/main/java/uk/ac/soton/comp2211/view/south/NotificationsPanel.java
@@ -1,31 +1,57 @@
 package uk.ac.soton.comp2211.view.south;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;
-
-import uk.ac.soton.comp2211.Observer;
 
 /**
  * This panel displays notifications.
  */
-public class NotificationsPanel extends JPanel implements Observer {
+public class NotificationsPanel extends JPanel {
 
-
+    JLabel notificationText;
+    int secondsToWait;
+    
     /**
      * Constructs a new notification panel.
      */
     public NotificationsPanel() {
         
-        JLabel notificationText = new JLabel("... content placeholder ...");
+        notificationText = new JLabel("... content placeholder ...");
+        
+        Font font = new Font("Courier", Font.BOLD,16);
+        notificationText.setFont(font);
         notificationText.setForeground(Color.RED);
         this.add(notificationText);
-    }
-
-    @Override
-    public void notifyUpdate() {
-        // TODO Auto-generated method stub
         
+        this.notificationUpdate("Obstacle sucessfully added");
     }
-
+    
+    /**
+     * This method displays a notification for a given number of seconds.
+     * @param text the notification to be displayed
+     */
+    public void notificationUpdate(String text) {
+        
+        secondsToWait = 2;
+        notificationText.setText(text);
+        
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+        
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                secondsToWait--;
+                if (secondsToWait == 0) {
+                    notificationText.setText("");
+                    exec.shutdown();
+                }
+            }
+        };   
+        exec.scheduleAtFixedRate(task, 1, 1, TimeUnit.SECONDS);       
+    }
 }
